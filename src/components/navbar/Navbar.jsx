cfa167201129fe6b7137f82677dc03580d85115a
Amanda Yu <amandayu255@gmail.com>
2024-02-26 01:41:53 -0800

    updated links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      {/* <div className="navbar__logo">Logo</div> */}
       <ul className="navbar__links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/signup">Sign Up</a></li>
-        <li><a href="/login">Login</a></li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/signup">Sign Up</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
