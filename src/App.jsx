acf910e2e3948240380d7abfa3a44a96df892df2
Amanda Yu <amandayu255@gmail.com>
2024-02-26 01:41:39 -0800

    updated links to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import SignUp from "./components/signup/Signup";
+import Login from "./components/login/Login";
 
-const App = () => {
+function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
