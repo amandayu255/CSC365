8061a6e75eabbc48a6e9239ca10f2e7b199aa356
amandayu255 <amandayu255@gmail.com>
2024-02-27 18:50:30 -0800

    added functions to handle search

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./SearchBar.css";
+import FiltersButton from "../filter/FiltersButton";
+import searchIcon from "./search-icon.svg";
 
 const SearchBar = ({ onSearch }) => {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   const handleInputChange = (e) => {
     setQuery(e.target.value);
@@ -12,16 +15,28 @@ const SearchBar = ({ onSearch }) => {
     onSearch(query);
   };
 
+  const handleSearchIconClick = () => {
+    console.log("Search icon clicked!");
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Search..."
-        value={query}
-        onChange={handleInputChange}
-      />
-      <button type="submit">Search</button>
+    <div>
+    <form className="search-bar-container" onSubmit={handleSubmit}>
+      <div className="search-input-container">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search..."
+          value={query}
+          onChange={handleInputChange}
+        />
+        <div className="search-icon-container" onClick={handleSearchIconClick}>
+          <img src={searchIcon} alt="Search" className="search-icon" />
+        </div>
+      </div>
     </form>
+    <FiltersButton />
+    </div>
   );
 };
 
--- src/components/storage/Storage.jsx
+++ src/components/storage/Storage.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import SearchBar from "../search/SearchBar";
-import FiltersButton from "../filter/FiltersButton";
 import "./Storage.css";
 
 const Storage = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    console.log("Search query:", query);
+  };
+
   return (
-    <div className="storage-container">
+    <div className="home-container">
       <header>
         <h1>Fridge/Pantry</h1>
       </header>
 
-      <SearchBar />
-      <FiltersButton />
+      <SearchBar onSearch={handleSearch} />
 
       <main>
         <Table stripped bordered hover size="sm">
@@ -25,6 +30,13 @@ const Storage = () => {
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <td>Ice cream</td>
+              <td>11</td>
+              <td>Annie</td>
+              <td>Poppy</td>
+            </tr>
+
             <tr>
               <td>Ice cream</td>
               <td>11</td>
