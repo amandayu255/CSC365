26489dd8f5fc0ea166b38921450f536735ceff4b
amandayu255 <amandayu255@gmail.com>
2024-03-14 10:25:35 -0700

    updated profile with shopping history, cooking history, and house id information

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -1,37 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Profile.css";
+import CookingHist from "../cookinghist/CookingHist";
+import ShoppingHist from "../shoppinghist/ShoppingHist";
+import Household from "../household/Household";
 
 const Profile = () => {
   return (
     <div className="profile-container">
-      <header>
-        <h1>My Profile</h1>
-      </header>
-      <main>
-        <div className="profile-info">
-          <img
-            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-            alt="Profile Picture"
-            className="profile-picture"
-          />
-          <div className="profile-details">
-            <h5 className="profile-name">John Doe</h5>
-            <p className="username">
-              <strong>Username: </strong>jdoe
-            </p>
-            <p className="profile-email">
-              <strong>Email:</strong> john.doe@example.com
-            </p>
-            <p className="most-used-ingredient">
-              <strong>Most Used Ingredient:</strong> Eggs
-            </p>
-            <p className="favorite-recipe">
-              <strong>Favorite Recipe:</strong> Fried rice
-            </p>
-          </div>
+  <header>
+    <h1>My Profile</h1>
+  </header>
+  <main>
+    <Link to="/Household"><button className="house-id">House ID</button></Link>
+    <div className="profile-info">
+      <div className="profile-details">
+        <h5 className="profile-name">John Doe</h5>
+        <p className="username">
+          <strong>Username: </strong>jdoe
+        </p>
+        <p className="profile-email">
+          <strong>Email:</strong> john.doe@example.com
+        </p>
+        <p className="favorite-recipe">
+          <strong>Favorite Recipe:</strong> Fried rice
+        </p>
+        <br></br>
+        <div className="buttons-container">
+          <Link to="/ShoppingHist">
+            <button>Shopping History</button>
+          </Link>
+          <Link to="/CookingHist">
+            <button>Cooking History</button>
+          </Link>
         </div>
-      </main>
+      </div>
     </div>
+  </main>
+</div>
+
   );
 };
 
