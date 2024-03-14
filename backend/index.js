28d6d0af21c561a3ee0d9abec57dfc5a4822d7fd
Amanda Yu <amandayu255@gmail.com>
2024-03-13 23:41:45 -0700

    added backend

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import mysql from "mysql"
+
+const app = express()
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "idk", // video at 13:00
+    database: "test"
+})
+
+app.get("/", (req, res) => {
+    res.json("hello this is the backend")
+})
+
+// table name 
+app.get("/recipes", (req, res) => {
+    const q = "SELECT * FROM recipes"
+    db.query(q, (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+app.listen(8800, () => {
+    console.log("Connected to backend!")
+})
